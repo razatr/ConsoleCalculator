005b57c5e0bba3d550e728745de93c7fdd727a1f
Legolas <razatr@yandex.ru>
2020-04-09 21:57:11 +0300

    the simplest calculator was created

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double a1, a2, result;
+			Console.WriteLine("Введите первое число");
+			while (true)
+			{
+				if (double.TryParse(Console.ReadLine(), out a1))
+					break;
+				Console.WriteLine("Введено не число, введите число");
+			}
+			Console.WriteLine("Введите второе число");
+			while (true)
+			{
+				if (double.TryParse(Console.ReadLine(), out a2))
+					break;
+				Console.WriteLine("Введено не число, введите число");
+			}
+			Console.WriteLine("Введите бинаруную операцию");
+			while (true)
+			{
+				switch (Console.ReadLine())
+				{
+					case "*":
+						result = a1*a2;
+						goto End;
+					case "/":
+						result = a1 / a2;
+						goto End;
+					case "+":
+						result = a1 + a2;
+						goto End;
+					case "-":
+						result = a1 - a2;
+						goto End;
+					default:
+						Console.WriteLine("Введена неизвестная операция. Введите заново.");
+						break;
+				}
+			}
+
+		End:
+			Console.WriteLine(result);
+			Console.Read();
+		}
+	}
+}
